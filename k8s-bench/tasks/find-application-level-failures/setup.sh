f07f808bd84acbfa4e757a84df2e296d09aaa735
Noah West <westnoah@google.com>
2025-05-30 18:04:05 -0700

    Create an eval for finding app-level failures in `kubectl logs`

---
+++ k8s-bench/tasks/find-application-level-failures/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+kubectl delete namespace app-level-fails-test --ignore-not-found
+kubectl create namespace app-level-fails-test
+kubectl create configmap eval-app-map --from-file=artifacts/eval-app.py --namespace=app-level-fails-test
+kubectl apply -f artifacts/eval-app-pod.yaml --namespace=app-level-fails-test
+
+# Wait a few seconds for pod to get at least 1 failure logged
+for i in {1..5}; do
+    sleep 1
+done 
