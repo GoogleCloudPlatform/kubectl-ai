c95ad483aebcb6d47f94ff2cd48c57d053ea92dd
Vivek Kanadiya <vivekkanadiya@google.com>
2025-06-09 21:17:13 +0000

    Updated namespace name to resize-pv and moved yaml files from setup.sh

--- k8s-bench/tasks/resize-pvc/setup.sh
+++ k8s-bench/tasks/resize-pvc/setup.sh
@@ -1,39 +1,6 @@
 #!/usr/bin/env bash
-kubectl delete namespace storage --ignore-not-found
-kubectl create namespace storage
+kubectl delete namespace resize-pv --ignore-not-found
+kubectl create namespace resize-pv
 
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: storage-pvc
-  namespace: storage
-spec:
-  accessModes:
-    - ReadWriteOnce
-  resources:
-    requests:
-      storage: 10Gi
-  storageClassName: standard # This assumes the name of your StorageClass is "expandable-sc"
-EOF
-
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: storage-pod
-  namespace: storage
-spec:
-  volumes:
-    - name: my-storage
-      persistentVolumeClaim:
-        claimName: storage-pvc # This assumes the name of your PersistentVolumeClaim is "storage-pvc"
-  containers:
-    - name: storage-user
-      image: nginx:alpine
-      volumeMounts:
-        - name: my-storage
-          mountPath: /usr/share/nginx/html
-      ports:
-        - containerPort: 80
-EOF
+kubectl apply -f ./storage-pvc.yaml
+kubectl apply -f ./storage-pod.yaml
